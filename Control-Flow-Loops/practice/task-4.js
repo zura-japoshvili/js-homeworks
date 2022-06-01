9d9a1019e01459701957e7751b00f6d687c2e411
zura-japoshvili <japoshvili777@gmail.com>
2022-06-01 20:35:49 +0400

    Oops, mistake in practice/task-4 (corrected)

--- Control-Flow-Loops/practice/task-4.js
+++ Control-Flow-Loops/practice/task-4.js
@@ -16,12 +16,16 @@
 //   console.log("2 or 3");
 // }
 
+const a = 0;
 switch(a){
     case 0 :
         console.log(0);
+        break;
     case 1 :
         console.log(1);
+        break;
     case 2 :
     case 3 : 
         console.log("2 or 3");
+        break;
 }
